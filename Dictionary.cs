00e3119b548330ab0762009eb9f4ccadcac68431
Kostyuchenko Evgen <74785068+yvgen12@users.noreply.github.com>
2021-09-09 13:44:23 +0300

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Dictionary<TKey, TValue>
+    {
+        private readonly List<TKey> key;                //field1
+        private readonly List<TValue> value;            //field2
+        public Dictionary()
+        {
+            key = new List<TKey>();
+            value = new List<TValue>();
+        }
+
+        public void AddDictionary(TKey k, TValue v)
+        {
+            key.Add(k);
+            value.Add(v);
+        }
+        public string this [int index] //простой индексатор
+        {
+            get { return key[index] + " " + value[index]; }
+        }
+        public string this[TKey index] // parametr
+        {
+            get
+            {
+                for (int i = 0; i<key.Count; i++)
+                {
+                    if ((string)(object)key[i] == (string)(object)index)
+                    {
+                        return "По ключу " + index.ToString().ToUpper() + " найдено значение: " + value[i].ToString().ToUpper();
+                    }
+                }
+                return "По ключу " + index + " не найдено значения ";
+            }
+        }
+        public int Length  // number records property 
+        {
+            get { return key.Count; }        
+        }
+        public override string ToString() // base class Object
+        {
+            string lineN = string.Empty;
+            for (int i = 0; i < key.Count; i++)
+            {
+                lineN += key[i] + " " + value[i] + "\n";
+            }
+            if (lineN != null)
+                return lineN;
+            return "In dictionary not meaning";
+        }
+
+         public void ReplaceValue(int k, TValue newV)                               // method change record
+         {
+            value.RemoveAt(k);
+            value.Insert(k, newV);
+         }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My dictionary");
+            var mydic = new Dictionary<string, string>();
+
+            mydic.AddDictionary("Contr1", "Kazahstan");
+            mydic.AddDictionary("Contr2", "Armenia");
+            mydic.AddDictionary("Contr3", "Uzbec");
+            mydic.AddDictionary("Contr4", "China");
+
+            Console.WriteLine(mydic.ToString());
+            Console.WriteLine("Dictionary have {0} records", mydic.Length);
+            Console.WriteLine(new string('-', 30));       //30 символов "-"
+            Console.WriteLine("1 -replacement record, 2 - display record");
+            Console.WriteLine("Enter your choice");
+            string mychoice = Console.ReadLine();
+            switch (mychoice)
+            {
+                case "1" :
+                    Console.WriteLine("Enter nuber record for replace");
+                    string mystr0 = Console.ReadLine();
+                    if (string.IsNullOrEmpty(mystr0))
+                        Console.WriteLine("You did not enter the number in the dictionary");
+                    else
+                    {
+                        int number = Convert.ToInt32(mystr0);
+                        Console.WriteLine(mydic[number - 1]);
+                        Console.WriteLine("Enter the new country value");
+                        string newcountry = Console.ReadLine();
+                        if (string.IsNullOrEmpty(mystr0))
+                        { 
+                            Console.WriteLine("You do not enter a value");
+                            return;
+                        }
+                        else
+                        {
+                            mydic.ReplaceValue(number - 1, newcountry);
+                            Console.WriteLine(mydic.ToString());
+                        }
+                    }
+                    break;
+
+                case "2" :
+                    Console.WriteLine("Enter the number of the entry in the dictionary:");
+                    string mystr = Console.ReadLine();
+                    if (string.IsNullOrEmpty(mystr))
+                        Console.WriteLine("You did not enter the number in the dictionary");
+                    else
+                    {
+                        int number = Convert.ToInt32(mystr);
+                        Console.WriteLine(mydic[number - 1]);
+                        return;
+                    }
+                    Console.WriteLine("Enter a value to search");
+                    string valp = Console.ReadLine();
+                    Console.WriteLine(mydic[valp]);
+                    break;
+            }
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
